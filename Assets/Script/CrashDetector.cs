c37122c6572d775cca06355783592efeefa4b166
TheLamboMilk <alexandru.tichilesteanu@gmail.com>
2021-11-29 22:50:54 +0200

    Audio Source

--- Assets/Script/CrashDetector.cs
+++ Assets/Script/CrashDetector.cs
@@ -7,11 +7,16 @@ public class CrashDetector : MonoBehaviour
 {
    [SerializeField] float delayScene = 1f;
    [SerializeField] ParticleSystem crashEffect;
+   [SerializeField] AudioClip crashSFX;
+   bool hasCrashed = false;
    void OnTriggerEnter2D(Collider2D other)
    {
-       if(other.tag == "Head" || other.tag == "Rocks")
+       if(other.tag == "Head" || other.tag == "Rocks" && !hasCrashed)
        {
+           hasCrashed = true;
+           FindObjectOfType<PlayerController>().DisableControls();
            crashEffect.Play();
+           GetComponent<AudioSource>().PlayOneShot(crashSFX);
            Invoke("ReloadScene",delayScene);
        }
    }
