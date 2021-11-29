86dfe1fde5566d5df62cac94bd2c79af170c8e66
TheLamboMilk <alexandru.tichilesteanu@gmail.com>
2021-11-29 22:09:53 +0200

    Dust

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostAmount = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
        rb2d = GetComponent<Rigidbody2D>(); 
+       surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+    
+    
+    void Update()
+    {
+        RotatePlayer(); 
+        RespondToBoost();
     }
 
-    // Update is called once per frame
-    void Update()
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostAmount;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,6 +44,6 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(-torqueAmount);
         }
-        
+
     }
 }
